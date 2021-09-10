17fa03a5e1de13d2290f86ff24fe4e96a48f8e4b
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-10 12:15:56 +0530

    Add files via upload

---
+++ scope_newAndComments.js
@@ -0,0 +1,9 @@
+//Single Line Comment
+/* Multi Line Comment*/
+var City = 'Kolkata';
+{
+    let City = 'Rampur';
+    console.log(City); //Local Variable
+}
+console.log(City);//Global Variable
+ 
